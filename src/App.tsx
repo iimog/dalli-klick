7b34527c0f5c66e0127dac6c1f12d742651bcf62
Markus Ankenbrand <markus@ankenbrand.me>
2021-09-29 22:21:42 +0200

    feat: repeat images only after showing all

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,12 @@ import mask7 from './img/masks/mask7.png';
 import mask8 from './img/masks/mask8.png';
 import _ from 'lodash';
 
+const allImages = _.shuffle([bild1, bild2, bild3, bild4, bild5, bild6])
+
 function App() {
-  const allImages = [bild1, bild2, bild3, bild4, bild5, bild6]
   const allMasks = [mask1, mask2, mask3, mask4, mask5, mask6, mask7, mask8]
   const [masks, setMasks] = useState(allMasks)
-  const [bild, setBild] = useState(_.sample(allImages))
+  const [bild, setBild] = useState(0)
 
   const revealTile = () => {
     if (masks.length > 0) {
@@ -30,23 +31,26 @@ function App() {
       setMasks(maskClone)
     } else {
       setMasks(allMasks)
-      setBild(_.sample(allImages))
+      setBild((bild + 1) % allImages.length)
     }
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+          {`Bild ${bild + 1}/${allImages.length}`}
+        </p>
         <div className="ratespiel" style={{ position: "relative", width: "80%" }} onClick={revealTile}>
-          <img src={bild} style={{
+          <img src={allImages[bild]} style={{
             maxWidth: "100%",
-            //maxHeight: "800px",
+            maxHeight: "800px",
             position: "relative"
           }} />
           {masks.map(m => <img src={m} style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }} />)}
         </div>
         <p>
-          {masks.length > 0 ? `Noch ${masks.length} Teile` : bild?.split('/')[3].split('.')[0].replaceAll("_", " ")}
+          {masks.length > 0 ? `Noch ${masks.length} Teile` : allImages[bild]?.split('/')[3].split('.')[0].replaceAll("_", " ")}
         </p>
       </header>
     </div>
